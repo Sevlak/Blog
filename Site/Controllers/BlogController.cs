78ba38ff44ea2cf335927ace4d4da75d6a619d8d
William Alves <willoandrade29@gmail.com>
2023-09-05 17:58:23 -0300

    feat: adds initial project files

---
+++ Site/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() //returns all my posts
+        {
+            return View();
+        }
+
+        public IActionResult Post(int id) //returns an specific post
+        {
+            return View();
+        }
+    }
+}
---
+++ Site/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Site.Models
+{
+    public class Post
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required DateTime CreatedOn { get; set; }
+        public required byte[] MarkdownFile { get; set; }
+        public string? Description { get; set; }
+    }
+}
