459be3672680f3566c89016fe639e83328902738
William Alves <willoandrade29@gmail.com>
2023-09-05 18:23:45 -0300

    feat: adds Post model

---
+++ Site/Migrations/20230905211809_CreateDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MarkdownFile = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
--- Site/Models/Post.cs
+++ Site/Models/Post.cs
@@ -1,11 +1,23 @@
-﻿namespace Site.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Site.Models
 {
     public class Post
     {
-        public required int Id { get; set; }
+        public required int PostId { get; set; }
         public required string Title { get; set; }
         public required DateTime CreatedOn { get; set; }
         public required byte[] MarkdownFile { get; set; }
         public string? Description { get; set; }
+    }
+
+    public class PostContext: DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=.\\Posts.db");
+        }
     }
 }
